75bec20500fe48e0ec360e86bd24991fa93aae5c
parkyujeong <yujeongb50@gmail.com>
2026-04-30 12:51:44 +0900

    도서대여프로그램(중간 업로드)2

--- parkyujeong/src/BookRentalProject/Dao/BookDAO.java
+++ parkyujeong/src/BookRentalProject/Dao/BookDAO.java
@@ -16,8 +16,6 @@ public class BookDAO {
     // DB와 연결 객체( DBConnection에서 받아옴 )
     Connection conn = new DBConnection().gConnection();
     
-    
-    private int bookId;
 
     //도서 검색 기능 ( 제목으로 검색 )
     public List<String> searchBook(String keyword) {
@@ -69,10 +67,64 @@ public class BookDAO {
             // UPDATE 실행 ( DB 값 변경 )
             ps.executeUpdate();
 
+            System.out.println("도서 ID : " + bookId + " →대여 불가 상태");
+
         } catch (Exception e) {
 
             // 오류 발생 시 출력
             e.printStackTrace();
         }
+    }
+          // 반납 시 → 대여 가능 처리
+         public void updateAvailable(int bookId) {
+
+        try {
+        String sql = "UPDATE BOOK SET IS_AVAILABLE = 'Y' WHERE BOOK_ID = ?";
+
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, bookId);
+
+        ps.executeUpdate();
+
+        System.out.println("도서 ID: " + bookId + " → 대여 가능 상태");
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
+
+    // 대여 가능 여부 확인
+    public boolean isAvailable(int bookId) {
+
+        boolean available = false;
+
+        try {
+            String sql = "SELECT IS_AVAILABLE FROM BOOK WHERE BOOK_ID = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, bookId);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                String result = rs.getString("IS_AVAILABLE");
+
+                // Y = 가능 / N = 불가능
+                available = result.equals("Y");
+            }
+
+            System.out.println("도서 ID: " + bookId);
+            System.out.println("👉 대여 가능 여부: " + (available ? "가능" : "대여중"));
+
+            rs.close();
+            ps.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return available;
+
+        
     }
 }
---
+++ parkyujeong/src/BookRentalProject/Service/Main.java
@@ -0,0 +1,112 @@
+//사용자 화면
+
+
+
+
+package BookRentalProject.Service;
+
+
+
+import java.util.Scanner;
+import BookRentalProject.Service.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        MemberService memberService = new MemberService();
+        RentalService rentalService = new RentalService();
+        BookService bookService = new BookService();
+
+        String loginId = null;
+
+        while (true) {
+
+            System.out.println("===== 도서 대여 시스템 =====");
+            System.out.println("1. 로그인");
+            System.out.println("2. 회원가입");
+            System.out.println("3. 도서 검색");
+            System.out.println("4. 도서 대여");
+            System.out.println("5. 도서 반납");
+            System.out.println("6. 마이페이지");
+            System.out.println("0. 종료");
+
+            System.out.print("선택 >> ");
+            int menu = sc.nextInt();
+            sc.nextLine();
+
+            
+            if (menu == 1) {
+                System.out.print("ID: ");
+                String id = sc.next();
+                System.out.print("PW: ");
+                String pw = sc.next();
+
+                boolean result = memberService.login(id, pw);
+
+                if (result) {
+                    loginId = id;
+                    System.out.println("로그인 성공");
+                } else {
+                    System.out.println("로그인 실패");
+                }
+
+            } else if (menu == 2) {
+
+                System.out.print("ID: ");
+                String id = sc.next();
+                System.out.print("PW: ");
+                String pw = sc.next();
+                System.out.print("이름: ");
+                String name = sc.next();
+                System.out.print("전화번호: ");
+                String phone = sc.next();
+                
+
+                memberService.register(id, pw, name, phone);
+                System.out.println("회원가입 완료");
+
+            } else if (menu == 3) {
+
+                System.out.print("검색어: ");
+                String keyword = sc.nextLine();
+
+                System.out.println(bookService.searchBook(keyword));
+
+            } else if (menu == 4) {
+
+                if (loginId == null) {
+                    System.out.println("로그인 먼저 하세요");
+                    continue;
+                }
+
+                System.out.print("책 번호: ");
+                int bookId = sc.nextInt();
+
+                try {
+                    rentalService.rentBook(loginId, bookId);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            } else if (menu == 5) {
+
+                System.out.print("대여번호: ");
+                int rentalId = sc.nextInt();
+
+                rentalService.returnBook(rentalId);
+
+            } else if (menu == 0) {
+                System.out.println("종료");
+                break;
+            }
+        }
+
+        sc.close();
+    }
+
+}
+    
+
